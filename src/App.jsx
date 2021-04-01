ca6b2c9ce70a73f66de3b67cc84b647785b06459
mkawashima <mkawashima@users.noreply.github.com>
2021-04-02 01:40:48 +0900

    構造の作成、CSS

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,44 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="input-area">
+        <input id="add-text" placeholder="TODOを入力" />
+        <button id="add-button">追加</button>
+      </div>
+
+      <div className="incomplete-area">
+        <p className="title">未完了TODO</p>
+        <ul id="incomplete-list">
+          <li className="list-row">
+            <div className="list-content">
+              <p>aaaaaaaaa</p>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          </li>
+          <li className="list-row">
+            <div className="list-content">
+              <p>ｂｂｂｂｂｂｂｂ</p>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+
+      <div className="complete-area">
+        <p className="title">完了したTODO</p>
+        <ul id="complete-list">
+          <li className="list-row">
+            <div className="list-content">
+              <p>ｂｂｂｂｂｂｂｂ</p>
+              <button>戻す</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 };
